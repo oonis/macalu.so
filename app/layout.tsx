d384805cadac420be1b523c753478ac120053561
Sam <30103396+oonis@users.noreply.github.com>
2025-04-27 17:16:21 -0400

    Add insights (#86)

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { RootProvider } from "fumadocs-ui/provider"
 import { Inter } from "next/font/google"
 import type { ReactNode } from "react"
 import { Metadata } from "next"
+import { SpeedInsights } from '@vercel/speed-insights/next';
+
 
 const inter = Inter({
   subsets: ["latin"],
@@ -40,6 +42,7 @@ export default function Layout({ children }: { children: ReactNode }) {
     <html lang="en" className={inter.className} suppressHydrationWarning>
       <body className="flex flex-col min-h-screen">
         <RootProvider>{children}</RootProvider>
+        <SpeedInsights />
       </body>
     </html>
   )
